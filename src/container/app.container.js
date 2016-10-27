c83292489cc95db7dff28cbd07ea1e5452c0c274
Priya Choudhary <priya@tothenew.com>
2016-10-27 14:12:08 +0530

    added function for send button

--- src/container/app.container.js
+++ src/container/app.container.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import {
-	Text,
-	View,
-	StyleSheet,
-    TouchableHighlight
+    Text,
+    View,
+    StyleSheet,
+    TouchableHighlight,
+    TextInput,
+    ListView
 } from 'react-native';
 import { connect } from 'react-redux';
 
@@ -12,69 +14,86 @@ let io = require('socket.io-client/socket.io');
 let url ='http://10.1.21.115:3000';
 
 export default class AppContainer extends Component {
-   constructor(props){
-    super(props)
-       this.state={
-           hell:''
-       };
-       this.socket = io(url, {jsonp:false});
-   }
+    constructor(props){
+        super(props);
+        var ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        let data = ds.cloneWithRows([]);
+        this.state={
+            data:data
+        };
+        this.socket = io(url, {jsonp:false});
+    }
     componentDidMount () {
-        this.socket.on('new message', function(msgObj){
-            this.setState({
-                hell:msgObj
-            });
-        });
 
-      }
-
-    async hell(){
-        console.log("in hell...")
-        this.socket.emit('send message', 'first')
     }
 
-	render() {
-		 return (
-              <View style={styles.container}>
-                  <TouchableHighlight onPress={()=> this.hell()}><Text>hellooooooo</Text></TouchableHighlight>
+    async hell(data){
+        var ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        console.log("in hell...");
+        this.socket.emit('send message', data);
+        this.socket.on('new message', (msObj)=>{
+            let data=[];
+            data.push(msObj);
+            let allData = ds.cloneWithRows(data || []);
+            this.setState({
+                data:allData
+            })
+        })
+    }
 
-                  <Text style={styles.welcome}>
-                  Welcome to React Native!
-                </Text>
-                <Text style={styles.instructions}>
-                  To get started, edit index.android.js
-                </Text>
-                <Text style={styles.instructions}>
-                  Double tap R on your keyboard to reload,{'\n'}
-                  Shake or press menu button for dev menu
-                </Text>
-
-              </View>
-            );
-	}
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={{flex: 1}}>
+                    <View style={{flex: .8}}>
+                        <ListView
+                            dataSource={this.state.data} enableEmptySections={true}
+                            renderRow={(rowData) => (
+                                 <View style={styles.item}>
+                                  <View style={styles.row}>
+                                   <Text>{rowData}</Text>
+                                    </View>
+                                     </View>
+                                     ) } />
+                    </View>
+                    <View style={{flex: .2}}>
+                        <TextInput
+                            style={styles.textEdit}
+                            onChangeText={(moreText) => this.setState({moreText})}
+                        />
+                        <TouchableHighlight onPress={()=> this.hell(this.state.)}><Text>Done</Text></TouchableHighlight>
+                    </View>
+                </View>
+            </View>
+        );
+    }
 }
 
 const mapStateToProps = function (state) {
-	return state;
+    return state;
 }
 const App = connect(mapStateToProps)(AppContainer);
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    textEdit: {
+       width:100,
+        height:100
+    }
 });
 
